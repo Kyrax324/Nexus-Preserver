f6e905e3059cb620c75afddd15a92a8211181739
kyrax324 <72662771+Kyrax324@users.noreply.github.com>
2023-02-03 17:20:50 +0800

    Update Recaptcha.php

--- Recaptcha-v3/Rules/Recaptcha.php
+++ Recaptcha-v3/Rules/Recaptcha.php
@@ -4,44 +4,54 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Http;
-use Log;
+
 class Recaptcha implements Rule
 {
-    const URL = 'https://www.google.com/recaptcha/api/siteverify';
+	public const URL = 'https://www.google.com/recaptcha/api/siteverify';
 
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+	public $errors = ['ReCaptcha error detected'];
 
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
-    public function passes($attribute, $value)
-    {
-        return Http::post((static::URL)."?".http_build_query([
-            'secret' => config('services.recaptcha.secret'),
-            'response' => $value,
-            'remoteip' => request()->ip()
-        ]))->json()['success'];
-    }
+	/**
+	 * Create a new rule instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+	}
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return 'The validation error message.';
-    }
+	/**
+	 * Determine if the validation rule passes.
+	 *
+	 * @param string $attribute
+	 * @param mixed  $value
+	 *
+	 * @return bool
+	 */
+	public function passes($attribute, $value)
+	{
+		$response = Http::post((static::URL) . '?' . http_build_query([
+			'secret' => config('services.recaptcha.secret'),
+			'response' => $value,
+			'remoteip' => request()->ip(),
+		]))->json();
+
+		if ($response['success'] == false)
+		{
+			$this->errors = array_merge($this->errors, $response['error-codes']);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Get the validation error message.
+	 *
+	 * @return string
+	 */
+	public function message()
+	{
+		return $this->errors;
+	}
 }
